a5c27c090a260491cb79a34fe1b5a6e7402ce174
Camilo Gil <camilo.gil2707@hotmail.com>
2023-05-24 23:51:38 -0500

    feat: Se crear el sql para ingresar categorias por defecto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (name,description)
+values('Entrantes o Aperitivos','Los entrantes son platos pequeños que se sirven al comienzo de una comida para abrir el apetito.'),
+      ('Platos Principales o Mains','Los platos principales son la parte central de una comida y suelen ser más abundantes y sustanciosos.'),
+      ('Guarniciones o Acompañamientos','Las guarniciones son platos secundarios que complementan los platos principales.'),
+      ('Postres','Los postres son platos dulces que se sirven al final de una comida para satisfacer el gusto por lo dulce.'),
+      ('Bebidas','Las bebidas son una categoría importante en un restaurante y pueden incluir una amplia gama de opciones.');
